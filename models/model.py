fa98758fde38ca8f7018a59772d4638d406110a5
Anish Govind <anishg24@gmail.com>
2020-02-01 14:13:52 -0800

    Created the main model file after understanding and tweaking my DataFrame.

--- models/model.py
+++ models/model.py
@@ -1,19 +1,61 @@
-# import keras as K
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout, Flatten
-# from keras.layers import Conv2D, MaxPooling2D
+import keras as K
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 
+BATCH_SIZE = 64
+NUM_CLASSES = 10
+EPOCHS = 12
+img_rows, img_cols = 128, 128
+input_shape = (img_rows, img_cols, 3)
+
 df = pd.read_csv("../data/dataframe.csv")
 
-y = df["Name"]
+# temp_X = np.array(df["Image Arrays"])
+# y = np.array(df["Name"])
+#
+# temp_X_train, temp_X_test, y_train, y_test = train_test_split(temp_X, y, test_size=0.3)
+
+
+# def breakdown(arr):
+#     X = []
+#     for category in arr:
+#         for photo in category:
+#             X.append(photo)
+#     return np.array(X)
+#
+#
+# X_train, X_test = breakdown(temp_X_train), breakdown(temp_X_test)
+
 X = np.array(df["Image Arrays"])
+y = np.array(df["Name"])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(NUM_CLASSES, activation='softmax'))
 
-# X_train = X_train.reshape(60000,28,28,1)
-# X_test = X_test.reshape(10000,28,28,1)
+model.compile(loss=K.losses.categorical_crossentropy,
+              optimizer=K.optimizers.Adadelta(),
+              metrics=['accuracy'])
 
-print(X_train[0])
+model.fit(X_train, y_train,
+          batch_size=BATCH_SIZE,
+          epochs=EPOCHS,
+          verbose=1,
+          validation_data=(X_test, y_test)
+          )
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
